e7d0e39773ddb263836b0dd12f0044c8c508806f
Wilson Pena <wilson.pena@imaginebeyond.com.br>
2020-08-25 17:36:34 +0100

    refactor(repository): remove unsusde classes

--- src/main/java/com/brasilprev/repository/CustomerRepository.java
+++
@@ -1,7 +0,0 @@
-package com.brasilprev.repository;
-
-import com.brasilprev.model.Customer;
-import org.springframework.data.repository.CrudRepository;
-
-public interface CustomerRepository extends CrudRepository<Customer, Long> {
-}
--- src/main/java/com/brasilprev/repository/OrderRepository.java
+++
@@ -1,7 +0,0 @@
-package com.brasilprev.repository;
-
-import com.brasilprev.model.Order;
-import org.springframework.data.repository.CrudRepository;
-
-public interface OrderRepository extends CrudRepository<Order, Long> {
-}
