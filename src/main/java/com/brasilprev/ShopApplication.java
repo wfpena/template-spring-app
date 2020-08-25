6e23681a02db0f08883eb7f0d874e3713fa39200
Wilson Pena <wilson.pena@imaginebeyond.com.br>
2020-08-25 17:15:31 +0100

    refactor: remove unused code and comment properties file

--- src/main/java/com/brasilprev/ShopApplication.java
+++ src/main/java/com/brasilprev/ShopApplication.java
@@ -2,12 +2,6 @@ package com.brasilprev;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.util.Arrays;
-
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.Bean;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @EntityScan("com.brasilprev.model")
