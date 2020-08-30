96a22d16786941f937b285144c7e8afe43dd6391
Wilson Pena <wilsonf.pena@gmail.com>
2020-08-30 13:54:40 -0300

    feat: adds swagger

--- src/main/java/com/brasilprev/ShopApplication.java
+++ src/main/java/com/brasilprev/ShopApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 
-@EntityScan("com.brasilprev.model")
+@EntityScan("com.brasilprev")
 @SpringBootApplication
 public class ShopApplication {
 
