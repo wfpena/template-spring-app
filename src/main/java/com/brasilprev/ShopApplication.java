a9f48f8a8ef1cd844a3f49f0a1a1851f745ace13
Wilson Pena <wilsonf.pena@gmail.com>
2020-09-02 08:09:19 -0300

    fix: entity scan

--- src/main/java/com/brasilprev/ShopApplication.java
+++ src/main/java/com/brasilprev/ShopApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 
-@EntityScan("com.brasilprev")
+@EntityScan("com.brasilprev.model")
 @SpringBootApplication
 public class ShopApplication {
 
