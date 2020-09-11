f4f4ebca8da6f85f0b029d4d7fd64837253d0115
Wilson Pena <wilsonf.pena@gmail.com>
2020-09-11 07:52:03 -0300

    fix: remove another line with comment

--- src/main/java/com/shopapp/SecurityConfig.java
+++ src/main/java/com/shopapp/SecurityConfig.java
@@ -14,7 +14,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
             .antMatchers("/").permitAll();
-            .anyRequest().authenticated()
+            // .anyRequest().authenticated()
             // .and()
             // .httpBasic()
             // .authenticationEntryPoint(authenticationEntryPoint);
