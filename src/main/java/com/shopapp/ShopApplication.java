70b1972ae681a1dc79c987636359dd8eb2a113b4
Wilson Pena <wilsonf.pena@gmail.com>
2020-09-13 08:59:35 -0300

    feat: add slf4j logger example

--- src/main/java/com/shopapp/ShopApplication.java
+++ src/main/java/com/shopapp/ShopApplication.java
@@ -4,11 +4,17 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 @EntityScan("com.shopapp.model")
 @SpringBootApplication
 public class ShopApplication {
+    
+    private static final Logger LOG = LoggerFactory.getLogger(ShopApplication.class);
 
-	public static void main(String[] args) {
-		SpringApplication.run(ShopApplication.class, args);
-	}
+    public static void main(String[] args) {
+        LOG.info("Starting Sprint Template Application");
+        SpringApplication.run(ShopApplication.class, args);
+    }
 }
