e1e280a1b9fd0250727ae1edb2481c2b0ae02c51
Wilson Pena <wilsonf.pena@gmail.com>
2020-09-18 08:57:48 -0300

    feat: add user and basic auth to api

--- src/main/java/com/shopapp/SecurityConfig.java
+++ src/main/java/com/shopapp/SecurityConfig.java
@@ -1,28 +1,43 @@
 package com.shopapp;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
- 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
-            .antMatchers("/**").permitAll();
-            // .anyRequest().permitAll();
-            // .and()
-            // .httpBasic()
-            // .authenticationEntryPoint(authenticationEntryPoint);
-        http.cors().and().csrf().disable();
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+            .inMemoryAuthentication()
+            .withUser("user")
+            .password(passwordEncoder()
+            .encode("pass"))
+            .authorities("ROLE_USER");
     }
 
-//    @Bean
-//    public PasswordEncoder passwordEncoder() {
-//        return new BCryptPasswordEncoder();
-//    }
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+            .cors().and().csrf().disable()
+            .authorizeRequests()
+            .antMatchers("/securityNone").permitAll()
+            .anyRequest().authenticated()
+            .and()
+            .httpBasic();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
+
