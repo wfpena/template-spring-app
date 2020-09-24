1161d2728bb6e2f18518be9f864f9625c1870e2b
Wilson Pena <wilsonf.pena@gmail.com>
2020-09-24 17:11:19 -0300

    feat: add unit test for order customer relation

--- src/test/java/com/shopapp/shop/OrderTest.java
+++ src/test/java/com/shopapp/shop/OrderTest.java
@@ -4,15 +4,21 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import java.net.URI;
 
+import org.json.JSONObject;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 
 import com.shopapp.controller.OrderController;
+import com.shopapp.model.Customer;
 import com.shopapp.model.Order;
 
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
@@ -43,6 +49,39 @@ public class OrderTest {
             .postForEntity(uri, order, Order.class);
 
         assertThat(result.getBody().getDescription()).isEqualTo(order.getDescription());
+    }
+    
+    @Test
+    public void insertsOrderWithCustomer() throws Exception {
+        final String baseUrl = "http://localhost:" + port;
+        URI uri = new URI(baseUrl + "/customers");
+        Customer customer = new Customer();
+        customer.setName("Customer with Order");
+        customer.setIndividualTaxpayerRegistry("09987602132");
+
+        ResponseEntity<Customer> result = this.restTemplate
+            .withBasicAuth("user", "pass")
+            .postForEntity(uri, customer, Customer.class);
+
+        assertThat(result.getBody().getName()).isEqualTo(customer.getName());
+
+        JSONObject request = new JSONObject();
+        request.put("description", "Order With Customer");
+        request.put("customer", new JSONObject().put("id", result.getBody().getId()));
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        HttpEntity<String> entity = new HttpEntity<String>(request.toString(), headers);
+        ResponseEntity<String> ordersResponse = restTemplate
+            .withBasicAuth("user", "pass")
+            .exchange(baseUrl + "/orders", HttpMethod.POST, entity, String.class);
+        
+        JSONObject createdOrder = new JSONObject(ordersResponse.getBody());
+        assertThat(createdOrder.get("description")).isEqualTo("Order With Customer");
+        
+        // JSONObject referencedCustomer = (JSONObject) createdOrder.getJSONObject("customer");
+        // System.out.println("HEREEEEEE");
+        // System.out.println(referencedCustomer);
     }
 
 }
