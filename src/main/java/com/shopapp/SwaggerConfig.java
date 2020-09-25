7afb331c8aacefad43467ecccfa94519138a1310
Wilson Pena <wilsonf.pena@gmail.com>
2020-09-25 08:40:10 -0300

    feat: add bean validator to swagger documentation

--- src/main/java/com/shopapp/SwaggerConfig.java
+++ src/main/java/com/shopapp/SwaggerConfig.java
@@ -2,9 +2,11 @@ package com.shopapp;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 
 import com.google.common.base.Predicate;
 
+import springfox.bean.validators.configuration.BeanValidatorPluginsConfiguration;
 import springfox.documentation.RequestHandler;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -14,6 +16,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
 @EnableSwagger2
+@Import(BeanValidatorPluginsConfiguration.class)
 public class SwaggerConfig {
     @Bean
     public Docket api() {
